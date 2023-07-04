a2b5bb7ec72a1933c71d662f083175c714fe8b55
lonely24345 <112269961+lonely24345@users.noreply.github.com>
2023-07-04 15:14:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx0e46b92940fbb53c",
+  appsecret: "b2b15801e23b63a83087b0915d3a63db",
+  template_id: "1Srx580DvtCVFIus43wecENWIff3p6e-_JzEwFh_2Ag",
+  touser: "o0Zvp51tXHHypbqD0atx4ZWMVYq4",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  key: "70b1684b80ed460697b798a61bbc9910",
+  city: "宁波",
+  first_date: "2022-05-27",
 };
